1a7ce51be62377261625ab4344c29e5096035ebf
Cedric St-Jean <cedric.stjean@gmail.com>
2017-05-09 17:15:42 -0400

    Use tempdir to create temporary jld file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,17 @@ using PyCallJLD
 using Base.Test
 using PyCall, JLD
 
+tmp = joinpath(tempdir(), "temp.jld")
+
 const deque = pyimport("collections")["deque"]
 
 obj = [deque([1,2,3]), deque([4,5,6])]
-save("temp.jld", "x", obj)
-@test load("temp.jld", "x") == obj
+save(tmp, "x", obj)
+@test load(tmp, "x") == obj
 
-save("temp.jld", "x", PyObject("fé"))
-@test load("temp.jld", "x") == PyObject("fé")
+save(tmp, "x", PyObject("fé"))
+@test load(tmp, "x") == PyObject("fé")
 
 obj = PyObject(rand(UInt8, 1000))
-save("temp.jld", "x", obj)
-@test load("temp.jld", "x") == obj
+save(tmp, "x", obj)
+@test load(tmp, "x") == obj
