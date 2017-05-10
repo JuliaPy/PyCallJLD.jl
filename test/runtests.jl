5841b3020babe087a8b739a6eaaf42a604be18f7
Cedric St-Jean <cedric.stjean@gmail.com>
2017-05-09 21:48:48 -0400

    Delete the temporary directory

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,22 @@ using PyCallJLD
 using Base.Test
 using PyCall, JLD
 
-tmp = joinpath(tempdir(), "temp.jld")
+dir = tempdir()
+tmp = joinpath(dir, "temp.jld")
 
-const deque = pyimport("collections")["deque"]
+try
+    const deque = pyimport("collections")["deque"]
 
-obj = [deque([1,2,3]), deque([4,5,6])]
-save(tmp, "x", obj)
-@test load(tmp, "x") == obj
+    obj = [deque([1,2,3]), deque([4,5,6])]
+    save(tmp, "x", obj)
+    @test load(tmp, "x") == obj
 
-save(tmp, "x", PyObject("fé"))
-@test load(tmp, "x") == PyObject("fé")
+    save(tmp, "x", PyObject("fé"))
+    @test load(tmp, "x") == PyObject("fé")
 
-obj = PyObject(rand(UInt8, 1000))
-save(tmp, "x", obj)
-@test load(tmp, "x") == obj
+    obj = PyObject(rand(UInt8, 1000))
+    save(tmp, "x", obj)
+    @test load(tmp, "x") == obj
+finally
+    rm(dir, force=true, recursive=true)
+end
